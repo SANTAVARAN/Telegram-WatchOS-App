a0b1800e579a004aaa60fac2bafe82a68f041e25
SANTAVARAN <SANTAVARAN@users.noreply.github.com>
2023-01-20 00:55:20 +0300

    Initial Commit

---
+++ Telegram Watch Watch App/SignInScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Telegram Watch Watch App
+//
+//  Created by SANTAVARAN on 19.01.2023.
+//
+
+import SwiftUI
+
+struct SignInScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInScreen()
+    }
+}
